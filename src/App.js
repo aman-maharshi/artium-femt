e517021b9d43c5b859939c16daa1dce81e286457
aman-maharshi <amanm4harshi@gmail.com>
2023-02-01 19:23:34 +0530

    init

---
+++ src/App.js
@@ -0,0 +1,16 @@
+function App() {
+    console.log(process.env.REACT_APP_API_ID)
+
+    return (
+        <div>
+            <h1 className="text-3xl font-bold underline">Hello world!</h1>
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum
+                culpa nostrum pariatur et a dicta tenetur aspernatur dolore
+                excepturi rerum?
+            </p>
+        </div>
+    )
+}
+
+export default App
